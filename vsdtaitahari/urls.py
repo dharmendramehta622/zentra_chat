a875966c63fe58e79ff2cd369289c87d2390e5e1
kodemafia008 <kodemafia008@gmail.com>
2024-06-26 00:03:10 +0545

    news setup done

---
+++ vsdtaitahari/urls.py
@@ -0,0 +1,42 @@
+"""vsdtaitahari URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from drf_spectacular.views import SpectacularAPIView,SpectacularRedocView,SpectacularSwaggerView
+
+admin.site.site_header = 'VDSTA Itahari Admin'                    # default: "Django Administration"
+admin.site.index_title = 'Dashbaord Area'                 # default: "Site administration"
+admin.site.site_title = 'Welcome to VDSTA Itahari Admin Portal'
+
+router = DefaultRouter()
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include('apps.website.urls')),
+    path('news',include('apps.news.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/',
+         SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/',
+         SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
